dcfdd6d304e2552f7116be9a752e9674bc05aa13
mariorch22 <marioraach01@gmail.com>
2026-05-12 23:06:48 +0200

    feat: VectorStore concept and InMemoryStore

---
+++ src/store/concept.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <concepts>
+#include <cstddef>
+
+#include "core/types.h"
+
+namespace vexdb {
+
+template <typename T>
+concept VectorStore = requires(const T store, Offset offset) {
+    { store.get_vector(offset) } -> std::same_as<const float*>;
+    { store.size() } -> std::same_as<std::size_t>;
+    { store.dimensions() } -> std::same_as<Dim>;
+};
+
+}  // namespace vexdb
---
+++ src/store/in_memory_store.cpp
@@ -0,0 +1,30 @@
+#include "store/in_memory_store.h"
+
+#include <cassert>
+
+namespace vexdb {
+
+InMemoryStore::InMemoryStore(Dim dim) : dim_(dim) {
+    assert(dim > 0);
+}
+
+Offset InMemoryStore::add_vector(const float* data) {
+    Offset offset = static_cast<Offset>(data_.size() / dim_);
+    data_.insert(data_.end(), data, data + dim_);
+    return offset;
+}
+
+const float* InMemoryStore::get_vector(Offset offset) const {
+    assert(offset < size());
+    return data_.data() + static_cast<std::size_t>(offset) * dim_;
+}
+
+std::size_t InMemoryStore::size() const {
+    return data_.size() / dim_;
+}
+
+Dim InMemoryStore::dimensions() const {
+    return dim_;
+}
+
+}  // namespace vexdb
